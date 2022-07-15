bedfd5a9c4fa265da3d32d8d2adcb999d76f581c
parkvsmin <sdfjkl658@naver.com>
2022-07-15 14:09:56 +0900

    11

--- src/com/iu/main/Main.java
+++ src/com/iu/main/Main.java
@@ -1,15 +1,12 @@
 package com.iu.main;
 
-import com.iu.terran.Marine;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Marine m1 = new Marine();
-		m1.setHp(30);
-		m1.info();
-		m1.move();
-
+		
 	}
-
+	
 }
+
+
